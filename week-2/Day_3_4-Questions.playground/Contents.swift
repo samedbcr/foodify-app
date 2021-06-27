5e890955c78f4c58d2ba77c2386c667df25fa5a6
samedbcr <samedbcrr@gmail.com>
2021-06-27 18:43:55 +0300

    Add day3-question1 solution

--- week-2/Day_3_4-Questions.playground/Contents.swift
+++ week-2/Day_3_4-Questions.playground/Contents.swift
@@ -1,3 +1,34 @@
 import Foundation
 
-var str = "Hello, playground"
+/**
+ Day-3 Question-1
+ MARK: Girilen bir sayının asal olup olmadığını bulan bir extension yazınız
+ */
+extension Int {
+    func isPrime() -> Bool {
+        var number = self
+
+        if self < 2 {
+            return false
+        }
+
+        if self == 2 {
+            return true
+        }
+
+        if self % 2 == 0 {
+            return false
+        }
+        // reduce number limit in order to optimization
+        number = Int(sqrt(Double(number)))
+
+        for i in stride(from: 3, through: number, by: 2) {
+            if self % i == 0 {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+7.isPrime() // true
