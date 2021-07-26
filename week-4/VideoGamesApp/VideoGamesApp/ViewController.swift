91ff6c8213dc34d25627c211b74287c84baea1e8
samedbcr <samedbcrr@gmail.com>
2021-07-26 18:52:29 +0300

    Create Video Games App

---
+++ week-4/VideoGamesApp/VideoGamesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoGamesApp
+//
+//  Created by Samed Biçer on 26.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+        
+    }
+
+
+}
+
